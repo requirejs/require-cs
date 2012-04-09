60d89e5d858121b75018f86a10961a385ab07631
jrburke <jrburke@gmail.com>
2012-04-09 12:12:30 -0700

    Forgot to up version number for build file.

--- csBuild.js
+++ csBuild.js
@@ -1,5 +1,5 @@
 /**
- * @license csBuild 0.3.0+ Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
+ * @license csBuild 0.4.0 Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/require-cs for details
  */

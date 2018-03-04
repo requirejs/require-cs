20c25e3b9250586cf0d64624daef7a4ee02a37d0
Kumar Harsh <khs@playlyfe.com>
2018-03-05 03:07:53 +0530

    Fix security vulnerability warning

--- demoserver.js
+++ demoserver.js
@@ -3,8 +3,7 @@ var send = require('send');
 var url = require('url');
 
 var app = http.createServer(function(req, res){
-  send(req, url.parse(req.url).pathname)
-  .root(__dirname)
+  send(req, url.parse(req.url).pathname, { root: __dirname })
   .pipe(res);
 });
 app.listen(8337, '127.0.0.1');
